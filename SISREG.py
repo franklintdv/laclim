1c55847952975d1e52d152b84a4642cb29a68040
Franklin Vasconcelos <franklintdv@gmail.com>
2024-01-19 09:29:59 -0300

    Atualizar o SISREG.py
    O arquivo gerado estava dando erro, coloquei no código para criar o delimitador ";" que é o que é usado no arquivo baixado.

--- SISREG.py
+++ SISREG.py
@@ -14,8 +14,8 @@ dados_totais = []
 for nome_arquivo in os.listdir(diretorio):
     if nome_arquivo.endswith('.csv'):
         caminho_completo = os.path.join(diretorio, nome_arquivo)
-        with open(caminho_completo, 'r', encoding='utf-8', newline='') as arquivo_csv:
-            leitor_csv = csv.reader(arquivo_csv)
+        with open(caminho_completo, 'r', newline='') as arquivo_csv:
+            leitor_csv = csv.reader(arquivo_csv, delimiter=';')  # Use ponto e vírgula como separador
             # Ignora o cabeçalho nos arquivos subsequentes (a partir do segundo)
             if dados_totais:
                 next(leitor_csv)
@@ -23,9 +23,8 @@ for nome_arquivo in os.listdir(diretorio):
             dados_totais.extend(leitor_csv)
 
 # Escreve os dados totais no arquivo resultante
-with open(arquivo_resultante, 'w', encoding='utf-8', newline='') as arquivo_resultante_csv:
-    escritor_csv = csv.writer(arquivo_resultante_csv)
+with open(arquivo_resultante, 'w', newline='') as arquivo_resultante_csv:
+    escritor_csv = csv.writer(arquivo_resultante_csv, delimiter=';')  # Use ponto e vírgula como separador
     # Cabeçalho, se necessário
     # escritor_csv.writerow(["Coluna1", "Coluna2", ...])
     escritor_csv.writerows(dados_totais)
-
